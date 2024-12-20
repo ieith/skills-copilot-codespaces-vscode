52272842eae103d6f53802a9fac5f3d52df06110
Dewi <106343438+ieith@users.noreply.github.com>
2024-12-20 20:49:15 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,5 @@
 // create web server
-// create a route for comments
-// create a route for comments/new
-// create a route for comments/:id
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
-// create a route for comments/:id/edit
+
 
 var express = require('express');
 var router = express.Router();
